93bc5fda46c47b01c2696c5ac514b7972f4d79a5
7minutech <elijohnson1107@gmail.com>
2025-09-27 18:52:36 -0500

    feat: post request route

--- get_test.sh
+++ get_test.sh
@@ -1,4 +1,8 @@
-echo "Request to /employee"
+echo "Request to GET /employee"
 curl -s http://localhost:8080/employee | jq
-echo "Request to /employee/id"
+echo "Request to GET /employee/id"
 curl -s http://localhost:8080/employee/1 | jq
+echo "Request to POST /employee"
+curl -X POST http://localhost:8080/employee \
+    -H "Content-Type: application/json" \
+    -d '{"name": "Eli", "salary": "1000", "age": "21"}'
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import fs from 'fs'
 
 const app = express();
+app.use(express.json());
 const port = 8080;
 
 const pkg = JSON.parse(fs.readFileSync('package.json'))
@@ -28,6 +29,42 @@ app.get("/employee/:id?", (req, res) => {
     }
 })
 
+app.post("/employee", (req, res) => {
+    res.set("Content-Type", "application/json")
+    if (req.get("Content-Type") != "application/json"){
+        console.log("error: POST request was not json")
+        res.send({success: false})
+        return
+    }
+    if (!(valid_employee)){
+        console.log("error: Post request employee json was not valid")
+        res.send({success: false})
+        return
+    }
+    let index = employee_db.length
+    let next_id = index + 1
+    let newEmployee = req.body
+    newEmployee.id = next_id
+    employee_db.push(newEmployee)
+    res.send({"success": true, "employee": employee_db[index]})
+})
+
 app.listen(port, () => {
 console.log(`Express ${version} Listening on Port ${port}`);
 });
+
+function valid_employee(employee) {
+    if (!("name" in employee)){
+        console.log("Request json does not have name")
+        return false
+    }
+    if (!("salary" in employee)){
+        console.log("Request json does not have salary")
+        return false
+    }
+    if (!("age" in employee)){
+        console.log("Request json does not have age")
+        return false
+    }
+    return true
+}
